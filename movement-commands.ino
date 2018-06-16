610fa0d4cb7d3b0642fd6047e4bee7700e16323d
Paul Qua <paulwqua@gmail.com>
2018-06-15 20:52:56 -0700

    Change myservo to just servo

--- movement-commands.ino
+++ movement-commands.ino
@@ -1,17 +1,17 @@
 #include <Servo.h> //include the servo library
-Servo myservo1;  // create servo object to control a servo
-Servo myservo2;
-Servo myservo3;
-Servo myservo4;
+Servo servo1;  // create servo object to control a servo
+Servo servo2;
+Servo servo3;
+Servo servo4;
 
 void setup() {
-  myservo1.attach(8);//Main arm servo left
-  myservo2.attach(9);//Main arm servo right
-  myservo3.attach(11);//Rotation Servo
-  myservo4.attach(10);//Top Arm servo
+  servo1.attach(8);//Main arm servo left
+  servo2.attach(9);//Main arm servo right
+  servo3.attach(11);//Rotation Servo
+  servo4.attach(10);//Top Arm servo
 }
 
 void loop() {
   //place your servo commands and delays here
-  //myservo#.write(servo position)
+  //servo#.write(servo position)
 }
