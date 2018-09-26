a43963da803b6d7c8e1a00c372e8a675e2dfb832
Paul Qua <paulwqua@gmail.com>
2018-09-26 14:57:28 -0500

    Change names and comments

--- master-slave.ino
+++ master-slave.ino
@@ -1,41 +1,45 @@
-#include <Servo.h>
+#include <Servo.h> // import servo library
 
-Servo servo1; // main arm servo left
-Servo servo2; // mairn arm servo right
-Servo servo3; // rotation servo
-Servo servo4; // top arm servo
+Servo mainArmLeft; // main arm servo left
+Servo mainArmRight; // main arm servo right
+Servo topArm; // top arm servo
+Servo rotation; // rotation servo or base servo
 
-int potpin3 = 3; // analog pin used to connect the servo1 & 2 potentiometer
-int potpin2 = 2; // analog pin used to connect the servo3 potentiometer
-int potpin1 = 1; // analog pin used to connect the servo4 potentiometer
+int analogPin3 = 3; // analog pin used to connect the main arm potentiometer
+int analogPin2 = 2; // analog pin used to connect the rotation potentiometer
+int analogPin1 = 1; // analog pin used to connect the topArm potentiometer
 
-int val;  // variable to read the value from the base analog pin
-int val1; // variable to read the value from the shoulder analog pin
-int val2; // variable to read the value from the elbow analog pin
+  // pot is short for potentiometer
+int potValue3; // variable to store the value from the main arm potentiometer
+int potValue2; // variable to store the value from the rotation potentiometer
+int potValue1; // variable to store the value from the top arm potentiometer
+
 
 void setup()
 {
-    servo1.attach(8);     // servo1 to pin 8 on arduino
-    servo2.attach(9); // servo2 to pin 9 on arduino
-    servo3.attach(11);    // servo3 to pin 11 on arduino
-    servo4.attach(10);  // servo4 to pin 10 on arduino
+    mainArmLeft.attach(8); // connect mainArmLeft servo to pin 8 on arduino
+    mainArmRight.attach(9); // connect mainArmRight servo to pin 9 on arduino
+    topArm.attach(10); // connect topArmServo to pin 10 on arduino
+    rotation.attach(11); // connect rotation servo to pin 11 on arduino
+
+
 }
 
 void loop()
 {
-    val = analogRead(potpin3);       // reads the value of the base potentiometer (value between 0 and 1023)
-    val = map(val, 0, 1023, 0, 179); // scale it to use it with the base servo (value between 0 and 180)
-    servo1.write(val);
-    servo2.write(180-val);// sets the base servo position according to the scaled value
-    delay(15);                       // waits for the base servo to get there
+    potValue3 = analogRead(analogPin3); // reads the analog value of the main arm potentiometer (value between 0 and 1023)
+    potValue3 = map(potValue3, 0, 1023, 0, 180); // convert, scale anaolog value to digital servo value for main arm servos (value between 0 and 180)
+    mainArmLeft.write(potValue3); // sets the mainArmLeft servo position according to the converted, scaled value
+    mainArmRight.write(180-potValue3); // reverses mainArmRight servo
+    delay(15); // waits for the main arm servos to get to position
 
-    val1 = analogRead(potpin2);        // reads the value of the shoulder potentiometer (value between 0 and 1023)
-    val1 = map(val1, 0, 1023, 0, 179); // scale it to use it with the shoulder servo (value between 0 and 180)
-    servo3.write(180-val1);              // sets the shoulder servo position according to the scaled value
-    delay(15);                         // waits for the shoulder servo to get there
+    potValue2 = analogRead(analogPin2); // reads the analog value of the rotation potentiometer (value between 0 and 1023)
+    potValue2 = map(potValue2, 0, 1023, 0, 180); // convert, scale anaolog value to digital servo value for rotation servo (value between 0 and 180)
+    rotation.write(potValue2); // sets the rotation servo position according to the converted, scaled value
+    delay(15); // waits for the rotation servo to get to position
 
-    val2 = analogRead(potpin1);        // reads the value of the elbow potentiometer (value between 0 and 1023)
-    val2 = map(val2, 0, 1023, 0, 179); // scale it to use it with the elbow servo (value between 0 and 180)
-    servo4.write(180-val2);                 // sets the elbow servo position according to the scaled value
-    delay(15);                         // waits for the elbow servo to get there
+    potValue1 = analogRead(analogPin1); // reads the analog value of the topArm potentiometer (value between 0 and 1023)
+    potValue1 = map(potValue1, 0, 1023, 0, 180); // convert, scale anaolog value to digital servo value for topArm servo (value between 0 and 180)
+    topArm.write(potValue1); // sets the topArm servo position according to the converted, scaled value
+    delay(15); // waits for the topArm servo to get to position
 }
